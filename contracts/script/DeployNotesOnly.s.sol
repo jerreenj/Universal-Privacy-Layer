f3f1127fe0c40279040b29809918b247839f7798
jerreenj <jerreen08@gmail.com>
2026-07-16 21:20:36 +0530

    style: forge fmt DeployNotesOnly.s.sol

--- contracts/script/DeployNotesOnly.s.sol
+++ contracts/script/DeployNotesOnly.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 import {Script, console2} from "forge-std/Script.sol";
 import {ConfidentialNotes} from "../src/ConfidentialNotes.sol";
+
 contract DeployNotesOnly is Script {
     function run() external {
         address verifier = 0x9DBDB7cC200B83Fa6C9673857058bC86Bf77773C;
